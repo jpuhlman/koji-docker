75f5c769e423195f0ffd60593fcc1b0a786ad1d3
jenkins <jenkins@mvista.com>
2019-07-24 22:34:30 -0700

    add hostenv.sh

---
+++ install-scripts/hostenv.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+
+for each in (open("/proc/1/environ").read().split('\x00')):
+	if len(each.split("=")) == 2:
+		var = each.split("=")[0]
+		val = each.split("=")[1]
+		print("export %s='%s'" % (var,val))
+	else:
+		print (each)
