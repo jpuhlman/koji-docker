637999fb615131a359ab483cc0eafb5f27b01379
jenkins <jpuhlman@mvista.com>
2019-08-18 10:35:30 -0700

    Add build containers script

---
+++ build-scripts/build-containers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+TOPDIR=$(dirname "$SCRIPT_DIR")
+source "$SCRIPT_DIR"/parameters.sh
+
+build_container () {
+	dockerfile=$1
+	imageName=$2
+	version=$3
+	sudo docker build -f $dockerfile . -t $imageName:$version
+	hash=$(sudo docker images | grep ^$imageName | grep $version | head -n 1 | while read n v h d; do echo $h; done)
+        echo hash = $hash
+	sudo docker tag $hash $imageName:latest
+}
+
+cd $TOPDIR
+$SCRIPT_DIR/build-rpm.sh
+
+build_container $KOJI_CONTAINER_DOCKERFILE $KOJI_CONTAINER_NAME $CONTAINER_VERSIONS
+build_container $KOJIBUILDER_CONTAINER_DOCKERFILE $KOJIBUILDER_CONTAINER_NAME $CONTAINER_VERSIONS
+
