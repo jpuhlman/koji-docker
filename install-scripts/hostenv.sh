c0b40015fc96ed47d1f8f71e2c834b3453f4ba60
jenkins <jpuhlman@mvista.com>
2019-10-08 17:11:52 -0700

    Add @ to space translation.

--- install-scripts/hostenv.sh
+++ install-scripts/hostenv.sh
@@ -2,9 +2,10 @@
 
 
 for each in (open("/proc/1/environ").read().split('\x00')):
-	if len(each.split("=")) == 2:
-		var = each.split("=")[0]
-		val = each.split("=")[1]
-		print("export %s='%s'" % (var,val))
-	else:
-		print (each)
+    if len(each.split("=")) == 2:
+        var = each.split("=")[0]
+        val = each.split("=")[1]
+        val = val.replace("@"," ")
+        print("export %s='%s'" % (var,val))
+    else:
+        print (each)
